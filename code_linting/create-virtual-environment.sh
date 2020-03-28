b55bdd8cd55e8b43a2b5917a614ed5581922c33e
Maciej Bak <maciej.bak@unibas.ch>
2020-03-28 21:50:44 +0100

    add code linting env recipe

---
+++ code_linting/create-virtual-environment.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+###############################################################################
+#
+#   AUTHOR: Maciej_Bak
+#   AFFILIATION: Swiss_Institute_of_Bioinformatics
+#   CONTACT: wsciekly.maciek@gmail.com
+#   CREATED: 27-03-2020
+#   LICENSE: GPL_v3.0
+#
+###############################################################################
+
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+conda env create --file $CWD/packages.yaml -p $CWD/env
