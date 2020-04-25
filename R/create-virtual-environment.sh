c5829523d7ead83286f4063e4336140ee9d17596
Maciej Bak <maciej.bak@unibas.ch>
2020-04-25 22:39:08 +0200

    fix stringi issue, add tidymodels, test all, OK

--- R/create-virtual-environment.sh
+++ R/create-virtual-environment.sh
@@ -17,3 +17,8 @@ conda env create --file $CWD/packages.yaml -p $CWD/env --quiet
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate $CWD/env
 R -e "library('tinytex'); tinytex::install_tinytex()"
+
+# install dll compiled against the OS / does not work from conda
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate $CWD/env
+R -e "install.packages('https://cran.r-project.org/src/contrib/Archive/stringi/stringi_1.4.5.tar.gz', repos=NULL, type='source')"
