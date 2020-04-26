148f0137b3b48b75db4e8b38c8cc1c7b8809d23f
Maciej Bak <maciej.bak@unibas.ch>
2020-04-26 23:52:40 +0200

    add strngi issue description

--- R/create-virtual-environment.sh
+++ R/create-virtual-environment.sh
@@ -18,7 +18,10 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate $CWD/env
 R -e "library('tinytex'); tinytex::install_tinytex()"
 
-# install dll compiled against the OS / does not work from conda
+# rstudio from conda forge requires stringi-1.4.3 which is not compiled
+# properly. The problem is fixed in stringi-1.4.6 from conda-forge, however
+# rstudio=1.1.456 from r requires older version.
+# Install dll compiled against the OS from R
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate $CWD/env
 R -e "install.packages('https://cran.r-project.org/src/contrib/Archive/stringi/stringi_1.4.5.tar.gz', repos=NULL, type='source')"
