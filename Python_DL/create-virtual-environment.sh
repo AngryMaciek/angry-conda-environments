2988d9baa21ce97b6d9a37e9e5e342a1c21c21b2
Maciej Bak <wsciekly.maciek@gmail.com>
2020-04-27 17:05:29 +0200

    add Python env for DL

---
+++ Python_DL/create-virtual-environment.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+###############################################################################
+#
+#   AUTHOR: Maciej_Bak
+#   AFFILIATION: Swiss_Institute_of_Bioinformatics
+#   CONTACT: wsciekly.maciek@gmail.com
+#   CREATED: 27-04-2020
+#   LICENSE: MIT
+#
+###############################################################################
+
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+conda env create --file $CWD/packages.yaml -p $CWD/env --quiet
